3948d1ff34a26da0d9dfe113e8c3375d3e66104c
sryung <sryung1225@gmail.com>
2023-10-07 18:53:39 +0900

    [📚] ChatGPT로 웹개발 완전정복 3주차 : Flask로 로또 추천 사이트 만들기

---
+++ 0.ready/4.Flask/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+import random
+
+@app.route("/")
+def home():
+    name = "이령"
+    lotto = [16, 18, 20, 23, 32, 43] # 내 로또 번호
+
+    # 랜덤 로또 번호
+    def generate_lotto_numbers():
+        numbers = random.sample(range(1, 46), 6) # 1-46 숫자 중 6개 선택
+        return sorted(numbers) # 오름차순 정렬
+
+    random_lotto = generate_lotto_numbers()
+
+    # 내 로또 번호 & 랜덤 로또 번호의 일치율
+    def count_common_elements(list1, list2):
+      common_elements = set(list1) & set(list2) # 두 리스트의 공통 숫자 추출
+      return len(common_elements) # 공통 숫자들의 수
+
+    common_count = count_common_elements(lotto, random_lotto)
+
+    # context 선언
+    context = {
+        "name": name,
+        "lotto": lotto,
+        "random_lotto": random_lotto,
+        "common_count": common_count,
+    }
+
+    return render_template("index.html", data=context)
+    # 아무것도 하지 않은 상태에서는 index.html 렌더링
+    # 만들어진 context를 data라는 이름으로 html에 넘겨줌
+
+
+@app.route("/mypage") # /mypage 루트 선언
+def mypage():
+    return "This is My Page!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)

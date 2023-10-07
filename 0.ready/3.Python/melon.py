b38528b6a1669b92ea2a75819e46e298cb792565
sryung <sryung1225@gmail.com>
2023-10-07 16:06:48 +0900

    [📚] ChatGPT로 웹개발 완전정복 3주차 : 음악 웹 스크래핑

---
+++ 0.ready/3.Python/melon.py
@@ -0,0 +1,18 @@
+# 멜론뮤직 스크래핑
+from bs4 import BeautifulSoup
+import requests
+
+# bs4 시작코드
+url = "https://www.melon.com/chart/index.htm"
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get(url, headers=headers)
+soup = BeautifulSoup(data.text, 'html.parser')
+
+trs = soup.select(".service_list_song > table > tbody > tr")
+for tr in trs:
+    rank = tr.select_one("span.rank").text
+    title = tr.select_one('.rank01 > span > a').text
+    artist = tr.select_one('.rank02 > a').text
+    image = tr.select_one('img')['src']
+    print(rank, artist, "-", title)

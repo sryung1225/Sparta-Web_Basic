71b71ebfdd64203f18069aa6b5c7fdb3d0652d29
sryung <sryung1225@gmail.com>
2023-10-09 20:55:40 +0900

    [📚] ChatGPT로 웹개발 완전정복 4주차 : DB

---
+++ 0.ready/5.DB/app.py
@@ -0,0 +1,25 @@
+# SQLAlchemy 준비 코드
+from flask import Flask
+import os
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, 'database.db')
+
+db = SQLAlchemy(app)
+
+# 테이블 생성 코드
+class Song(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False)
+    artist = db.Column(db.String, nullable=False)
+    title = db.Column(db.String, nullable=False)
+    image_url = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return f'{self.artist} {self.title} 추천 by {self.username}'
+
+with app.app_context():
+    db.create_all()

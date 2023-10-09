172d8c6b7b90813a8166a87caca141936f12ebd8
sryung <sryung1225@gmail.com>
2023-10-09 21:53:57 +0900

    [📚] ChatGPT로 웹개발 완전정복 4주차 : 숙제 : 멜로디쉐어에 멜론 Top100 차트 연결

--- 0.ready/Project/app.py
+++ 0.ready/Project/app.py
@@ -4,6 +4,9 @@
 1. render_template : html파일을 가져와서 보여준다
 '''
 from flask import Flask, render_template
+from bs4 import BeautifulSoup
+import requests
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -19,7 +22,23 @@ def home():
 
 @app.route("/music")
 def music():
-    return render_template("music.html")
+    # bs4 시작코드
+    url = "https://www.melon.com/chart/index.htm"
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(url, headers=headers)
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    melon_data = []
+    trs = soup.select(".service_list_song > table > tbody > tr")
+    for tr in trs:
+        rank = tr.select_one('.rank').text
+        title = tr.select_one('.rank01 > span > a').text
+        artist = tr.select_one('.rank02 > a').text
+        image = tr.select_one('img')['src']
+        melon_data.append({'rank': rank, 'artist': artist, 'title': title, 'image': image})
+
+    return render_template("music.html", data=melon_data)
 
 @app.route("/iloveyou/<name>/")
 def iloveyou(name):
